c714a81442dba80953549489ce93077a86d2eb9d
Luis Antelo <luis.antm@hotmail.com>
2022-09-13 16:50:36 -0400

    dialogflow-cx

--- src/dialogflow.controller.js
+++ src/dialogflow.controller.js
@@ -1,32 +1,64 @@
-const  { SessionsClient } = require('@google-cloud/dialogflow');
+const dialogflowSE = require('@google-cloud/dialogflow');
+const dialogflowCX = require('@google-cloud/dialogflow-cx'); 
 const serviceAccount = require('../service-account.json');
+
+const PROJECT_ID = serviceAccount.project_id;
+const LOCATION = 'us-central1';
+const AGENT_ID = 'attention-agent';
+const LANGUAGE_CODE = 'es';
 
 
 async function SendToBot(senderID, message) {
+    return dialogFlowCX(senderID, message);
+};
+
+
+async function dialogFlowCX(senderID, message) {
     const sessionId = senderID;
-
-    const sessionClient = new SessionsClient( { credentials: serviceAccount } );
-
-    const sessionPath = sessionClient.projectAgentSessionPath('topicos-chatbot-lfqe', sessionId);
+    const sessionClient = new dialogflowCX.SessionsClient({ credentials: serviceAccount });
+    const sessionPath = sessionClient.projectLocationAgentSessionPath(
+        PROJECT_ID,
+        LOCATION,
+        AGENT_ID,
+        sessionId
+    );
 
     const request = {
         session: sessionPath,
         queryInput: {
             text: {
                 text: message,
-                languageCode: 'es',
+                languageCode: LANGUAGE_CODE,
             },
         },
     };
 
-    const responses = await sessionClient.detectIntent(request);
-    
+    const responses = await sessionClient.detectIntent(request); 
     const result = responses[0].queryResult;
+    var responseMessage = result.responseMessages;
 
+
+    return responseMessage[0].text.text;
+}
+
+async function dialogFlowSE(senderID, message) {
+    const sessionId = senderID;
+    const sessionClient = new dialogflowSE.SessionsClient( { credentials: serviceAccount } );
+    const sessionPath = sessionClient.projectAgentSessionPath(PROJECT_ID, sessionId);
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,
+                languageCode: LANGUAGE_CODE,
+            },
+        },
+    };
+    const responses = await sessionClient.detectIntent(request); 
+    const result = responses[0].queryResult;
     console.log(result);
-
     var textResponse = result.fulfillmentText;
     return textResponse;
-};
+}
 
 module.exports = {SendToBot};
